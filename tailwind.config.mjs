bb5bef492848a647c3b2caa1a718d37c06c07c7d
KewneWhimsy <josshanel@gmail.com>
2024-11-26 15:16:59 +0100

    base logo, mode sombre et boutons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,13 @@ export default {
 				// couleurs définie dans le global.css
 				text: 'var(--textcolor)',
 				bg: 'var(--bgcolor)',
-				mid:'var(--midcolor)'
+				mid:'var(--midcolor)',
+				btn:'var(--btncolor)'
 			},
-
+			borderColor: {
+				text: 'var(--textcolor)',
+				mid:'var(--midcolor)', 
+			},
 			fontFamily: {
 				text: ["system-ui", "sans-serif", "Arial"], // police de base ou autre police lisible et universel
 				heading: ["sans-serif", "Tahoma", "Verdana", "system-ui", "Arial"], // police des titres
